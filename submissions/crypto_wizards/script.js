42a4a82cf5b3fe9872b097ad94eb7ecc8618af50
Ayush <aaayushmalik283@gmail.com>
2026-03-24 04:41:57 +0530

    importing weights and bias

--- submissions/crypto_wizards/script.js
+++ submissions/crypto_wizards/script.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", init);
 function init() {
-    loadData();
+  loadData();
 }
 
 //___________________________________________API_________________________________________________________________
@@ -38,94 +38,122 @@ const key = "CG-zBNS7Y2qU3cHf5twpN9CJ3pg";
 // fetchData("bitcoin");
 
 async function fetchglobal() {
-    try{
-        const res = await fetch(`https://api.coingecko.com/api/v3/global?x_cg_api_key=${key}`);
-        const data = await res.json();
-        console.log("Global Data loaded :",data);
-        return data;
-    }catch(err){
-        console.error("Failed to fetch CoinGecko data:", err);
-    }
+  try {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/global?x_cg_api_key=${key}`,
+    );
+    const data = await res.json();
+    console.log("Global Data loaded :", data);
+    return data;
+  } catch (err) {
+    console.error("Failed to fetch CoinGecko data:", err);
+  }
 }
 async function fetchTrending() {
-    try{
-        const res = await fetch(`https://api.coingecko.com/api/v3/search/trending?x_cg_api_key=${key}`);
-        const data = await res.json();
-        const trending = data.coins.slice(0, 10);
-        console.log("trending Data loaded :",trending);
-        return trending;
-    }catch(err){
-        console.error("Failed to fetch CoinGecko data:", err);
-    }
+  try {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/search/trending?x_cg_api_key=${key}`,
+    );
+    const data = await res.json();
+    const trending = data.coins.slice(0, 10);
+    console.log("trending Data loaded :", trending);
+    return trending;
+  } catch (err) {
+    console.error("Failed to fetch CoinGecko data:", err);
+  }
 }
 async function fetchGainersLosers() {
-    try{
-        const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=250&page=1&price_change_percentage=24h&x_cg_api_key=${key}`);
-        const data = await res.json();
-        const valid = data.filter(c => c.price_change_percentage_24h !== null);
-        const gainers = [...valid]
-            .sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h)
-            .slice(0, 11);
+  try {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=250&page=1&price_change_percentage=24h&x_cg_api_key=${key}`,
+    );
+    const data = await res.json();
+    const valid = data.filter((c) => c.price_change_percentage_24h !== null);
+    const gainers = [...valid]
+      .sort(
+        (a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h,
+      )
+      .slice(0, 11);
 
-        const losers = [...valid]
-            .sort((a, b) => a.price_change_percentage_24h - b.price_change_percentage_24h)
-            .slice(0, 11);
-        console.log("Top Gainers:", gainers);
-        console.log("Top Losers:", losers);
-        return {gainers,losers};
-    }catch(err){
-        console.error("Failed to fetch CoinGecko data:", err);
-    }
+    const losers = [...valid]
+      .sort(
+        (a, b) => a.price_change_percentage_24h - b.price_change_percentage_24h,
+      )
+      .slice(0, 11);
+    console.log("Top Gainers:", gainers);
+    console.log("Top Losers:", losers);
+    return { gainers, losers };
+  } catch (err) {
+    console.error("Failed to fetch CoinGecko data:", err);
+  }
 }
 const coins = [
-"aave",
-"binancecoin",
-"bitcoin",
-"cardano",
-"chainlink",
-"cosmos",
-"crypto-com-chain",
-"dogecoin",
-"eos",
-"ethereum",
-"iota",
-"litecoin",
-"monero",
-"nem",
-"polkadot",
-"solana",
-"stellar",
-"tether",
-"tron",
-"uniswap",
-"usd-coin",
-"wrapped-bitcoin",
-"ripple"
+  "aave",
+  "binancecoin",
+  "bitcoin",
+  "cardano",
+  "chainlink",
+  "cosmos",
+  "crypto-com-chain",
+  "dogecoin",
+  "eos",
+  "ethereum",
+  "iota",
+  "litecoin",
+  "monero",
+  "nem",
+  "polkadot",
+  "solana",
+  "stellar",
+  "tether",
+  "tron",
+  "uniswap",
+  "usd-coin",
+  "wrapped-bitcoin",
+  "ripple",
 ];
 async function fetchList(id = coins) {
-    try{
-        const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=${id}&x_cg_api_key=${key}`);
-        const data = await res.json();
-        console.log("Coins data :",data);
-        return data;
-    }catch(err){
-        console.error("Failed to fetch CoinGecko data:", err);
-    }
+  try {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=${id}&x_cg_api_key=${key}`,
+    );
+    const data = await res.json();
+    data.sort((a, b) => a.name.localeCompare(b.name));
+    console.log("Coins data :", data);
+    return data;
+  } catch (err) {
+    console.error("Failed to fetch CoinGecko data:", err);
+  }
+}
+async function fetchOHLC(id = coins) {
+  try {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/ids=${id}/ohlc`,
+    );
+    const data = await res.json();
+    // data.sort((a, b) => a.name.localeCompare(b.name));
+    console.log("OHLC data :", data);
+    return data;
+  } catch (err) {
+    console.error("Failed to fetch CoinGecko data:", err);
+  }
 }
 
 async function loadData() {
   try {
-    const [global, trending,gainersLosers, coins] = await Promise.all([
+    const [global, trending, gainersLosers, coins, OHLC , models] = await Promise.all([
       fetchglobal(),
       fetchTrending(),
       fetchGainersLosers(),
-      fetchList()
+      fetchList(),
+      fetchOHLC(),
+      loadModels(),
     ]);
     const { gainers, losers } = gainersLosers;
     renderGlobal(global);
     renderTrending(trending);
-    renderGainersLosers(gainers,losers);
-    renderList(coins);
+    renderGainersLosers(gainers, losers);
+    renderList(coins,OHLC, models);
   } catch (err) {
     console.error("Failed to fetch CoinGecko data:", err);
   }
@@ -133,59 +161,69 @@ async function loadData() {
 
 // //___________________________________________Render_________________________________________________________________
 function setColor(el, value) {
-    if (value > 0) {
+  if (value > 0) {
     el.style.color = "var(--green)";
-    } else if (value < 0) {
+  } else if (value < 0) {
     el.style.color = "var(--red)";
-    } else {
+  } else {
     el.style.color = "gray";
-    }
+  }
 }
 function renderGlobal(global) {
-    document.getElementById("hero2-market").querySelector("p").textContent = global.data.total_market_cap.inr.toLocaleString("en-IN", { style: "currency", currency: "INR" });
-    document.getElementById("hero2-volume").querySelector("p").textContent = global.data.total_volume.inr.toLocaleString("en-IN", { style: "currency", currency: "INR" });
-    document.getElementById("hero2-dominance").querySelector("p").textContent = global.data.market_cap_percentage.btc.toFixed(2) + "% BTC";
-    document.getElementById("hero2-active").querySelector("p").textContent = global.data.active_cryptocurrencies;
+  document.getElementById("hero2-market").querySelector("p").textContent =
+    global.data.total_market_cap.inr.toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
+    });
+  document.getElementById("hero2-volume").querySelector("p").textContent =
+    global.data.total_volume.inr.toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
+    });
+  document.getElementById("hero2-dominance").querySelector("p").textContent =
+    global.data.market_cap_percentage.btc.toFixed(2) + "% BTC";
+  document.getElementById("hero2-active").querySelector("p").textContent =
+    global.data.active_cryptocurrencies;
 
-    const el1 = document.getElementById("hero2-market").querySelector("span");
-    const el2 = document.getElementById("hero2-volume").querySelector("span");
-    value1 = global.data.market_cap_change_percentage_24h_usd;
-    value2 = global.data.volume_change_percentage_24h_usd;
-    el1.textContent = (value1 > 0 ? "+" : "") + value1.toFixed(2) + "%";
-    el2.textContent = (value2 > 0 ? "+" : "") + value2.toFixed(2) + "%";
-    setColor(el1, value1);
-    setColor(el2, value2);
+  const el1 = document.getElementById("hero2-market").querySelector("span");
+  const el2 = document.getElementById("hero2-volume").querySelector("span");
+  value1 = global.data.market_cap_change_percentage_24h_usd;
+  value2 = global.data.volume_change_percentage_24h_usd;
+  el1.textContent = (value1 > 0 ? "+" : "") + value1.toFixed(2) + "%";
+  el2.textContent = (value2 > 0 ? "+" : "") + value2.toFixed(2) + "%";
+  setColor(el1, value1);
+  setColor(el2, value2);
 }
-function renderTrending(trending){
-    const listEl = document.getElementById("hero3-list1");
-    listEl.innerHTML = "";
-    trending.forEach((coin,i) => {
-        const el = document.createElement("li");
-        el.innerHTML = `${i+1}. ${coin.item.name} <span>₹${coin.item.data.price.toFixed(2)}</span>`;
-        listEl.appendChild(el);
-    });
+function renderTrending(trending) {
+  const listEl = document.getElementById("hero3-list1");
+  listEl.innerHTML = "";
+  trending.forEach((coin, i) => {
+    const el = document.createElement("li");
+    el.innerHTML = `${i + 1}. ${coin.item.name} <span>₹${coin.item.data.price.toFixed(2)}</span>`;
+    listEl.appendChild(el);
+  });
 }
-function renderGainersLosers(gainers,losers){
-    const listEl1 = document.getElementById("hero3-list2");
-    listEl1.innerHTML = "";
-    gainers.forEach((coin,i) => {
-        const el = document.createElement("li");
-        el.innerHTML = `${i+1}. ${coin.name} <span>+${coin.price_change_percentage_24h.toFixed(2)}%</span>`;
-        listEl1.appendChild(el);
-    });
-    const listEl2 = document.getElementById("hero3-list3");
-    listEl2.innerHTML = "";
-    losers.forEach((coin,i) => {
-        const el = document.createElement("li");
-        el.innerHTML = `${i+1}. ${coin.name} <span>${coin.price_change_percentage_24h.toFixed(2)}%</span>`;
-        listEl2.appendChild(el);
-    });
-    // listEl1.querySelectorAll("span").forEach(el => setColor(el, 1));
-    // listEl2.querySelectorAll("span").forEach(el => setColor(el, -1));
+function renderGainersLosers(gainers, losers) {
+  const listEl1 = document.getElementById("hero3-list2");
+  listEl1.innerHTML = "";
+  gainers.forEach((coin, i) => {
+    const el = document.createElement("li");
+    el.innerHTML = `${i + 1}. ${coin.name} <span>+${coin.price_change_percentage_24h.toFixed(2)}%</span>`;
+    listEl1.appendChild(el);
+  });
+  const listEl2 = document.getElementById("hero3-list3");
+  listEl2.innerHTML = "";
+  losers.forEach((coin, i) => {
+    const el = document.createElement("li");
+    el.innerHTML = `${i + 1}. ${coin.name} <span>${coin.price_change_percentage_24h.toFixed(2)}%</span>`;
+    listEl2.appendChild(el);
+  });
+  // listEl1.querySelectorAll("span").forEach(el => setColor(el, 1));
+  // listEl2.querySelectorAll("span").forEach(el => setColor(el, -1));
 }
-function renderList(coins) {
-    const listEl = document.getElementById("list-list");
-    listEl.innerHTML = `<li id="list-el" class="list-el">
+function renderList(coins,OHLC, models) {
+  const listEl = document.getElementById("list-list");
+  listEl.innerHTML = `<li id="list-el" class="list-el">
                     <h4>Coin</h4>
                     <p>Price</p>
                     <p>24h%</p>
@@ -194,22 +232,77 @@ function renderList(coins) {
                     <p>Prediction</p>
                     <p>Confidence</p>
                 </li>`;
-    
-    coins.forEach((coin,i) => {
-        const el = document.createElement("li");
-        el.classList.add("list-el");
-        el.innerHTML = `<h4><img src="${coin.image}" alt="hell">${coin.name}</h4>
+
+  coins.forEach((coin, i) => {
+    const el = document.createElement("li");
+    el.classList.add("list-el");
+    el.innerHTML = `<h4><img src="${coin.image}" alt="hell">${coin.name}</h4>
                     <p>${coin.current_price.toLocaleString("en-IN", { style: "currency", currency: "INR" })}</p>
                     <p>${(coin.price_change_percentage_24h > 0 ? "+" : "") + coin.price_change_percentage_24h.toFixed(2) + "%"}</p>
                     <p>${coin.high_24h.toLocaleString("en-IN", { style: "currency", currency: "INR" })}</p>
                     <p>${coin.low_24h.toLocaleString("en-IN", { style: "currency", currency: "INR" })}</p>
                     <p><i class="fa-solid fa-caret-up"></i></p>
-                    <p>55%</p>`;
-        listEl.appendChild(el);
-    });
-    const listEls=document.querySelectorAll(".list-el");
-    listEls.forEach((el,i) =>{
-        if(i>0){
-            setColor(el.querySelectorAll("p")[1], parseFloat(el.querySelectorAll("p")[1].textContent));
-        }});
+                    <p>predict([],models[i])</p>`;
+    listEl.appendChild(el);
+  });
+  const listEls = document.querySelectorAll(".list-el");
+  listEls.forEach((el, i) => {
+    if (i > 0) {
+      setColor(
+        el.querySelectorAll("p")[1],
+        parseFloat(el.querySelectorAll("p")[1].textContent),
+      );
+    }
+  });
 }
+
+//___________________________________________ML_________________________________________________________________
+async function loadModels() {
+  const modelFiles = [
+    "ml_model/weights/coin_Aave.json",
+    "ml_model/weights/coin_BinanceCoin.json",
+    "ml_model/weights/coin_Bitcoin.json",
+    "ml_model/weights/coin_Cardano.json",
+    "ml_model/weights/coin_ChainLink.json",
+    "ml_model/weights/coin_Cosmos.json",
+    "ml_model/weights/coin_CryptocomCoin.json",
+    "ml_model/weights/coin_Dogecoin.json",
+    "ml_model/weights/coin_EOS.json",
+    "ml_model/weights/coin_Ethereum.json",
+    "ml_model/weights/coin_Iota.json",
+    "ml_model/weights/coin_Litecoin.json",
+    "ml_model/weights/coin_Monero.json",
+    "ml_model/weights/coin_NEM.json",
+    "ml_model/weights/coin_Polkadot.json",
+    "ml_model/weights/coin_Solana.json",
+    "ml_model/weights/coin_Stellar.json",
+    "ml_model/weights/coin_Tether.json",
+    "ml_model/weights/coin_Tron.json",
+    "ml_model/weights/coin_Uniswap.json",
+    "ml_model/weights/coin_USDCoin.json",
+    "ml_model/weights/coin_WrappedBitcoin.json",
+    "ml_model/weights/coin_XRP.json",
+  ];
+  const models = [];
+
+  for (const file of modelFiles) {
+    const response = await fetch(file);
+    if (!response.ok) {
+      throw new Error(`Model file failed: ${file} (${response.status})`);
+    }
+    const model = await response.json();
+    models.push(model);
+  }
+  console.log("ML Models loaded:", models);
+  return models;
+}
+function predict(features, model) {
+    let result = model.bias;
+    for (let i = 0; i < features.length; i++) {
+        result += features[i] * model.weights[i];
+    }
+
+    const prob = 1 / (1 + Math.exp(-result));
+    return prob > 0.5 ? "UP" : "DOWN";
+}
+
